9d8e6deea5aa68817117212047ba9642eb6a4a0b
FredGuarana <fguarana@gmail.com>
2023-05-12 23:03:38 -0300

    Frist part app - screen layout

--- app/src/main/java/com/example/droiddesserts/MainActivity.kt
+++ app/src/main/java/com/example/droiddesserts/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.droiddesserts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ivDonuts:ImageView = findViewById(R.id.ivDonuts)
+        val ivIcecream:ImageView = findViewById(R.id.ivIceCream)
+        val ivFroyo:ImageView = findViewById(R.id.ivFroyo)
+
+        ivDonuts.setOnClickListener{
+            Toast.makeText(this, "Donuts are glazed and sprintkled with candy.", Toast.LENGTH_LONG).show()
+
+        }
+
+        ivIcecream.setOnClickListener {
+            Toast.makeText(this,"Icem cream sanduwiches have chocolate and vanila filling",Toast.LENGTH_LONG).show()
+        }
+
+        ivFroyo.setOnClickListener {
+            Toast.makeText(this, "FroYo is premium serve frozenyogurt", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
